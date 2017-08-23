be5992db4ae6d14d36eca47f5cce9ce5716760d7
Scott Wittenburg <scott.wittenburg@kitware.com>
2017-08-23 10:31:31 -0600

    Try out a parallel build by itself to start out

--- scripts/dashboard/circle_GNU7_OpenMPI.cmake
+++ scripts/dashboard/circle_GNU7_OpenMPI.cmake
@@ -1,19 +1,19 @@
 # Client maintainer: chuck.atkins@kitware.com
 set(CTEST_SITE "CircleCI PR Build")
-set(CTEST_BUILD_NAME "$ENV{CIRCLE_BRANCH}_GNU4_NoMPI")
+set(CTEST_BUILD_NAME "$ENV{CIRCLE_BRANCH}_GNU7_OpenMPI")
 set(CTEST_BUILD_CONFIGURATION Release)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_BUILD_FLAGS "-k")
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
 set(dashboard_model Experimental)
-set(dashboard_root_name "Builds/GCC-4.8.5_NoMPI")
+set(dashboard_root_name "Builds/GCC-7.1.0_OpenMPI")
 
 set(ENV{CC}  gcc)
 set(ENV{CXX} g++)
 set(ENV{FC}  gfortran)
 
 set(dashboard_cache "
-ADIOS_USE_MPI:BOOL=OFF
+ADIOS_USE_MPI:BOOL=ON
 ADIOS_USE_BZip2:BOOL=ON
 ADIOS_USE_HDF5:BOOL=ON
 ADIOS_USE_DataMan_ZeroMQ:BOOL=ON
--- scripts/dashboard/runOnCircle.sh
+++ scripts/dashboard/runOnCircle.sh
@@ -10,15 +10,25 @@ mkdir -p ${BASEDIR}/../../../Logs
 
 DASHBOARD_CONFIGS="GNU7_NoMPI"
 
-# First do the system tools build without mpi
-CONFIG="GNU4_NoMPI"
+# # First do the system tools build without mpi
+# CONFIG="GNU4_NoMPI"
 
-# unload any modules just in case
+# # unload any modules just in case
+# module purge
+
+# echo "Running configuration ${CONFIG}"
+# LOG=${BASEDIR}/../../../Logs/${CONFIG}
+# ctest -S ${BASEDIR}/circle_${CONFIG}.cmake -VV 1>${LOG}.out 2>${LOG}.err
+
+# Next do the gnu7 parallel build
+CONFIG="GNU7_OpenMPI"
+
 module purge
+module load gnu7
+module load openmpi
+module load phdf5
+module load netcdf
 
 echo "Running configuration ${CONFIG}"
 LOG=${BASEDIR}/../../../Logs/${CONFIG}
 ctest -S ${BASEDIR}/circle_${CONFIG}.cmake -VV 1>${LOG}.out 2>${LOG}.err
-
-# Next do the gnu7 parallel build
-### Not yet implemented
