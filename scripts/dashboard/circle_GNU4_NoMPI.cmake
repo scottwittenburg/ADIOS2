29c6a443dd99e174840d11b473d697e87de3a14e
Scott Wittenburg <scott.wittenburg@kitware.com>
2017-08-24 13:36:46 -0600

    Clean up test case scripts

--- scripts/dashboard/circle_GNU4_NoMPI.cmake
+++ scripts/dashboard/circle_GNU4_NoMPI.cmake
@@ -5,8 +5,11 @@ set(CTEST_BUILD_CONFIGURATION Release)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_BUILD_FLAGS "-k")
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
+
 set(dashboard_model Experimental)
 set(dashboard_root_name "Builds/GCC-4.8.5_NoMPI")
+set(dashboard_binary_name "GNU4_NoMPI")             # Keep build trees separate for each build
+set(dashboard_git_branch $ENV{CIRCLE_BRANCH})       # Let dashboard know what branch CircleCI is testing
 
 set(ENV{CC}  gcc)
 set(ENV{CXX} g++)
@@ -18,10 +21,5 @@ ADIOS_USE_BZip2:BOOL=ON
 ADIOS_USE_HDF5:BOOL=ON
 ADIOS_USE_DataMan_ZeroMQ:BOOL=ON
 ")
-
-set(dashboard_binary_name "GNU4_NoMPI")             # Keep build trees separate for each build
-# set(CTEST_SOURCE_DIRECTORY "/home/adios2/adios2")   # Avoid checking out the source before building
-# set(CTEST_GIT_COMMAND "/usr/bin/true")              # Avoid the red update failures on the dashboard
-set(dashboard_git_branch $ENV{CIRCLE_BRANCH})       # Let dashboard know what branch CircleCI is testing
 
 include(${CMAKE_CURRENT_LIST_DIR}/adios_common.cmake)
