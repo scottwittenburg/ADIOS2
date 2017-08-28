b74424b9702f7f57ec77f9c76a20036df8813967
Scott Wittenburg <scott.wittenburg@kitware.com>
2017-08-28 10:10:09 -0600

    Test parallel build step

--- scripts/dashboard/runOnCircle.sh
+++ scripts/dashboard/runOnCircle.sh
@@ -3,7 +3,7 @@
 # Make sure we can find cmake
 . /home/adios2/.bashrc
 
-echo "Hello from runOnCircle.sh (node index = ${CIRCLE_NODE_INDEX}, total nodes = ${CIRCLE_NODE_TOTAL})"
+echo "runOnCircle.sh (node index = ${CIRCLE_NODE_INDEX}, total nodes = ${CIRCLE_NODE_TOTAL})"
 
 # Initialize modules system
 . /etc/profile.d/lmod.sh >/dev/null
@@ -11,36 +11,26 @@ echo "Hello from runOnCircle.sh (node index = ${CIRCLE_NODE_INDEX}, total nodes 
 BASEDIR=$(readlink -f $(dirname ${BASH_SOURCE}))
 cd ${BASEDIR}
 
-
-
 ARGUMENTS="$@"
 
 IFS=' '
 read -ra DASHSCRIPTS <<< "$ARGUMENTS"
 for i in "${DASHSCRIPTS[@]}"; do
-    # fileName=basename $i
-    # echo "Running dashboard script: ${fileName}"
     if [[ "$i" =~ scripts/dashboard/circle_([^\.]+) ]];
     then
         CONFIG="${BASH_REMATCH[1]}"
+
         echo "Running build: ${CONFIG}"
-        # mkdir -p ${BASEDIR}/../../../${CONFIG}/Logs
+
+        DASHBOARDDIR=${BASEDIR}/../../../${CONFIG}
+        LOGSDIR=${DASHBOARDDIR}/Logs
+        mkdir -p ${LOGSDIR}
+
+        export CTEST_DASHBOARD_ROOT="${DASHBOARDDIR}"
+
+        LOG=${LOGSDIR}/${CONFIG}
+		ctest -S ${BASEDIR}/circle_${CONFIG}.cmake -VV 1>${LOG}.out 2>${LOG}.err
     else
         echo "Unable to find build name in ${i}"
     fi
 done
-
-
-
-# Post a comment on the PR linking to the CDash results
-#. postComment.sh >/dev/null || true
-
-# Configure a set of dashboards to run
-# DASHBOARD_CONFIGS="GNU4_NoMPI GNU7_OpenMPI"
-
-# for CONFIG in ${DASHBOARD_CONFIGS}
-# do
-#   echo ${CONFIG}
-#   LOG=${BASEDIR}/../../../Logs/${CONFIG}
-#   ctest -S ${BASEDIR}/circle_${CONFIG}.cmake -VV 1>${LOG}.out 2>${LOG}.err
-# done
