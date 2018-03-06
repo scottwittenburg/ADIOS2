2cddf4861ffa95cef89d4774117edbf512780706
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-06 16:45:36 -0700

    Move toward letting ctest drive the clang-analyzer

--- scripts/travis/run-check.sh
+++ scripts/travis/run-check.sh
@@ -6,6 +6,16 @@ then
   exit 1
 fi
 
+### Would like a more recent version of cppcheck so that we can use
+### the --project option, but we will have to fetch and build that
+### ourselves.  It doesn't seem like all three builds needs to incur
+### this cost either, so I'm considering doing it in here.
+# CHECK_TARBALL_URL=
+# CHECK_EXE_ROOT_DIR=$HOME/cppcheck
+# cd $CHECK_EXE_ROOT_DIR
+# mkdir build install
+# curl -OL
+
 ANALYSIS_OUTPUT_DIR=$HOME/cppcheck-output
 CHECK_EXE=cppcheck
 mkdir ${ANALYSIS_OUTPUT_DIR}
--- scripts/travis/run-sa.sh
+++ scripts/travis/run-sa.sh
@@ -6,6 +6,16 @@ then
   exit 1
 fi
 
+### To let ctest drive this process so we can have results on CDash
+### I figured out how to avoid needing the scan-build driver.  Then
+### I just need a dashboard script that I can trigger from this
+### script.
+# find /usr/share/clang | grep ccc-analyzer$
+# find /usr/share/clang | grep c++-analyzer$
+# cmake -DCMAKE_CXX_COMPILER:FILEPATH=/usr/share/clang/scan-build-3.9/libexec/c++-analyzer -DCMAKE_C_COMPILER:FILEPATH=/usr/share/clang/scan-build-3.9/libexec/ccc-analyzer ../adios2Src/
+# export CCC_ANALYZER_ANALYSIS="-analyzer-checker alpha.security.ArrayBoundV2 -analyzer-checker alpha.security.MallocOverflow -analyzer-checker alpha.security.ReturnPtrRange -analyzer-checker alpha.security.taint.TaintPropagation -analyzer-checker alpha.unix.Chroot -analyzer-checker alpha.unix.PthreadLock -analyzer-checker alpha.unix.SimpleStream -analyzer-checker alpha.unix.Stream -analyzer-checker alpha.unix.cstring.BufferOverlap -analyzer-checker alpha.unix.cstring.NotNullTerminated -analyzer-checker alpha.unix.cstring.OutOfBounds"
+# make -j8
+
 ANALYSIS_OUTPUT_DIR=$HOME/scan-build-output
 SCAN_BUILD_EXE=scan-build-3.8
 mkdir ${ANALYSIS_OUTPUT_DIR}
