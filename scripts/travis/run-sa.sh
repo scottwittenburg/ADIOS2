35a611f0855b07e9d031151302e82589abe26459
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-06 19:03:38 -0700

    Add clang-analyzer dashboard script for Travis

--- scripts/travis/run-sa.sh
+++ scripts/travis/run-sa.sh
@@ -10,37 +10,42 @@ fi
 ### I figured out how to avoid needing the scan-build driver.  Then
 ### I just need a dashboard script that I can trigger from this
 ### script.
-# find /usr/share/clang | grep ccc-analyzer$
-# find /usr/share/clang | grep c++-analyzer$
-# cmake -DCMAKE_CXX_COMPILER:FILEPATH=/usr/share/clang/scan-build-3.9/libexec/c++-analyzer -DCMAKE_C_COMPILER:FILEPATH=/usr/share/clang/scan-build-3.9/libexec/ccc-analyzer ../adios2Src/
-# export CCC_ANALYZER_ANALYSIS="-analyzer-checker alpha.security.ArrayBoundV2 -analyzer-checker alpha.security.MallocOverflow -analyzer-checker alpha.security.ReturnPtrRange -analyzer-checker alpha.security.taint.TaintPropagation -analyzer-checker alpha.unix.Chroot -analyzer-checker alpha.unix.PthreadLock -analyzer-checker alpha.unix.SimpleStream -analyzer-checker alpha.unix.Stream -analyzer-checker alpha.unix.cstring.BufferOverlap -analyzer-checker alpha.unix.cstring.NotNullTerminated -analyzer-checker alpha.unix.cstring.OutOfBounds"
-# make -j8
+export CCC_ANALYZER=$(find /usr/share/clang | grep ccc-analyzer$)
+export CXX_ANALYZER=$(find /usr/share/clang | grep c++-analyzer$)
 
-ANALYSIS_OUTPUT_DIR=$HOME/scan-build-output
-SCAN_BUILD_EXE=scan-build-3.8
-mkdir ${ANALYSIS_OUTPUT_DIR}
+echo "CCC_ANALYZER=${CCC_ANALYZER}"
+echo "CXX_ANALYZER=${CXX_ANALYZER}"
 
-cd ${BUILD_DIR}
+CUSTOM_BUILD_NAME="${TRAVIS_PULL_REQUEST_BRANCH}_${TRAVIS_BUILD_NUMBER_clang-analyzer}"
+CTEST_SCRIPT="${SOURCE_DIR}/scripts/travis/travis_clang-analyzer.cmake"
 
-${SCAN_BUILD_EXE} -h
-${SCAN_BUILD_EXE} cmake ${SOURCE_DIR}
-${SCAN_BUILD_EXE} -plist-html --status-bugs -o ${ANALYSIS_OUTPUT_DIR} \
-  -enable-checker "alpha.security.ArrayBoundV2" \
-  -enable-checker "alpha.security.MallocOverflow" \
-  -enable-checker "alpha.security.ReturnPtrRange" \
-  -enable-checker "alpha.security.taint.TaintPropagation" \
-  -enable-checker "alpha.unix.Chroot" \
-  -enable-checker "alpha.unix.PthreadLock" \
-  -enable-checker "alpha.unix.SimpleStream" \
-  -enable-checker "alpha.unix.Stream" \
-  -enable-checker "alpha.unix.cstring.BufferOverlap" \
-  -enable-checker "alpha.unix.cstring.NotNullTerminated" \
-  -enable-checker "alpha.unix.cstring.OutOfBounds" \
-  make -j8
+extra_checkers=(
+  alpha.security.ArrayBoundV2
+  alpha.security.MallocOverflow
+  alpha.security.ReturnPtrRange
+  alpha.security.taint.TaintPropagation
+  alpha.unix.Chroot
+  alpha.unix.PthreadLock
+  alpha.unix.SimpleStream
+  alpha.unix.Stream
+  alpha.unix.cstring.BufferOverlap
+  alpha.unix.cstring.NotNullTerminated
+  alpha.unix.cstring.OutOfBounds
+)
 
-if [ $? -eq 0 ]
-then
-  exit 0
-fi
+ANALYSES=""
 
-exit 1
+for checker in "${extra_checkers[@]}"; do
+  ANALYSES="${ANALYSES} -analyzer-checker ${checker}"
+done
+
+echo "Extra checkers environment variable: ${ANALYSES}"
+
+export CCC_ANALYZER_ANALYSIS=${ANALYSES}
+
+steps=( update configure build )
+for step in "${steps[@]}"; do
+    echo "Running $step step"
+    ctest -VV -S ${CTEST_SCRIPT} -Ddashboard_full=OFF -Ddashboard_do_${step}=TRUE -DCTEST_BUILD_NAME=${CUSTOM_BUILD_NAME}
+done
+
---
+++ scripts/travis/travis_clang-analyzer.cmake
@@ -0,0 +1,35 @@
+# Client maintainer: chuck.atkins@kitware.com
+set(CTEST_SITE "Travis CI")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_FLAGS "-k -j8")
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
+
+set(dashboard_model Experimental)
+set(dashboard_binary_name "build_clang-analyzer")
+set(dashboard_track "Continuous Integration")
+
+set(CTEST_GIT_COMMAND "/usr/bin/git")
+set(CTEST_UPDATE_VERSION_ONLY TRUE)
+set(CTEST_SOURCE_DIRECTORY "$ENV{SOURCE_DIR}")
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}")
+
+set(ENV{CC}  "$ENV{CCC_ANALYZER}")
+set(ENV{CXX} "$ENV{CXX_ANALYZER}")
+# set(ENV{FC}  gfortran)
+
+# set(dashboard_cache "
+# ADIOS2_USE_ADIOS1:STRING=ON
+# ADIOS2_USE_BZip2:STRING=ON
+# ADIOS2_USE_DataMan:STRING=ON
+# ADIOS2_USE_Fortran:STRING=ON
+# ADIOS2_USE_HDF5:STRING=ON
+# ADIOS2_USE_MPI:STRING=OFF
+# ADIOS2_USE_Python:STRING=ON
+# ADIOS2_USE_ZFP:STRING=ON
+# ADIOS2_USE_ZeroMQ:STRING=ON
+# ZFP_ROOT:PATH=/opt/zfp/install
+# ADIOS1_ROOT:PATH=/opt/adios1/1.12.0/gnu48
+# ")
+
+include(${CMAKE_CURRENT_LIST_DIR}/../dashboard/adios_common.cmake)
