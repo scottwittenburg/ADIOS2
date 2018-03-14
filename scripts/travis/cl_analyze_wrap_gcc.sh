fa69640a766051f8b8c6047b5034e896b1fdc734
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-14 10:07:22 -0600

    Run clang-analyzer through wrappers too

---
+++ scripts/travis/cl_analyze_wrap_gcc.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ -z "${DO_CL_ANAlYZE}" ]
+then
+  gcc "$@"
+else
+  { RESULT=$(${CCC_ANALYZER} "$@" 2>&1 1>&3-) ;} 3>&1
+  echo "${RESULT}"
+  shopt -s nocasematch
+  if [[ "$RESULT" =~ "warning|error" ]]; then
+    exit 1
+  fi
+  exit 0
+fi
--- scripts/travis/run-sa.sh
+++ scripts/travis/run-sa.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
+set -ex
+
 if [ -z "${SOURCE_DIR}" ]
 then
   echo "Error: SOURCE_DIR is empty or undefined"
   exit 1
 fi
 
-### To let ctest drive this process so we can have results on CDash
-### I figured out how to avoid needing the scan-build driver.  Then
-### I just need a dashboard script that I can trigger from this
-### script.
-export CCC_ANALYZER=$(find /usr/share/clang | grep ccc-analyzer$)
-export CXX_ANALYZER=$(find /usr/share/clang | grep c++-analyzer$)
+# First download recent clang+llvm binaries
+mkdir -p ${HOME}/clang_llvm/scan_output
+pushd ${HOME}/clang_llvm
+wget http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+tar xvf clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz
+
+export CCC_ANALYZER=$(find ${HOME}/clang_llvm | grep ccc-analyzer$)
+export CXX_ANALYZER=$(find ${HOME}/clang_llvm | grep c++-analyzer$)
+export WRAPPED_CC="${SOURCE_DIR}/scripts/travis/cl_analyze_wrap_gcc.sh"
+export WRAPPED_CXX="${SOURCE_DIR}/scripts/travis/cl_analyze_wrap_gplusplus.sh"
 
 echo "CCC_ANALYZER=${CCC_ANALYZER}"
 echo "CXX_ANALYZER=${CXX_ANALYZER}"
@@ -41,11 +47,21 @@ done
 
 echo "Extra checkers environment variable: ${ANALYSES}"
 
+# Environment variables used by analyzers
 export CCC_ANALYZER_ANALYSIS=${ANALYSES}
+export OUTPUT_DIR=${HOME}/clang_llvm/scan_output
+
+popd
 
 steps=( update configure build )
 for step in "${steps[@]}"; do
     echo "Running $step step"
+    CL_ANAlYZE=""
+    if [ "$step" == "build" ]
+    then
+        CL_ANAlYZE="TRUE"
+    fi
+    export DO_CL_ANAlYZE=${CL_ANAlYZE}
     ctest -VV -S ${CTEST_SCRIPT} -Ddashboard_full=OFF -Ddashboard_do_${step}=TRUE -DCTEST_BUILD_NAME=${CUSTOM_BUILD_NAME}
 done
 
