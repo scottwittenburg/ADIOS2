d50e2c73640f2a9e38ee7c54ab8576ccc935cb05
Scott Wittenburg <scott.wittenburg@kitware.com>
2018-03-14 11:23:50 -0600

    Fix clang-analyzer regex check

--- scripts/travis/cl_analyze_wrap_gplusplus.sh
+++ scripts/travis/cl_analyze_wrap_gplusplus.sh
@@ -7,7 +7,7 @@ else
   { RESULT=$(${CXX_ANALYZER} "$@" 2>&1 1>&3-) ;} 3>&1
   echo "${RESULT}"
   shopt -s nocasematch
-  if [[ "$RESULT" =~ "warning|error" ]]; then
+  if [[ "$RESULT" =~ warning|error ]]; then
     exit 1
   fi
   exit 0
