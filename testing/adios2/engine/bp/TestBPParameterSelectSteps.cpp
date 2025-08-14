4f562bdf0004593d673660e9d9ee05a152e9de3b
Scott Wittenburg <scott.wittenburg@kitware.com>
2025-08-14 11:17:55 -0600

    Add coverage of all aggregator types to some bp5-only tests

--- testing/adios2/engine/bp/TestBPParameterSelectSteps.cpp
+++ testing/adios2/engine/bp/TestBPParameterSelectSteps.cpp
@@ -21,6 +21,7 @@
 #include "../SmallTestData.h"
 
 std::string engineName; // comes from command line
+std::string aggType = "TwoLevelShm"; // overridden on command line
 constexpr std::size_t NSteps = 10;
 const std::size_t Nx = 10;
 using DataArray = std::array<int32_t, Nx>;
@@ -80,6 +81,7 @@ public:
         std::string filename = "ParameterSelectSteps" + std::to_string(mpiSize) + ".bp";
         adios2::IO ioWrite = adios.DeclareIO("TestIOWrite");
         ioWrite.SetEngine(engineName);
+        ioWrite.SetParameter("AggregationType", aggType);
         adios2::Engine engine = ioWrite.Open(filename, adios2::Mode::Write);
         // Number of elements per process
         const std::size_t Nx = 10;
@@ -195,6 +197,7 @@ TEST_P(BPParameterSelectStepsP, Stream)
     std::string filename = "ParameterSelectStepsStream" + std::to_string(mpiSize) + ".bp";
     adios2::IO ioWrite = adios.DeclareIO("TestIOWrite");
     ioWrite.SetEngine(engineName);
+    ioWrite.SetParameter("AggregationType", aggType);
     adios2::Engine writer = ioWrite.Open(filename, adios2::Mode::Write);
 
     adios2::IO ioRead = adios.DeclareIO("TestIORead");
@@ -302,6 +305,11 @@ int main(int argc, char **argv)
     if (argc > 1)
     {
         engineName = std::string(argv[1]);
+    }
+
+    if (argc > 2)
+    {
+        aggType = std::string(argv[2]);
     }
 
     result = RUN_ALL_TESTS();
